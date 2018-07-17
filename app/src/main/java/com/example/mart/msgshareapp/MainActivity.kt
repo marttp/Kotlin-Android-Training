939f3b8798709f5d359a2d8d75c4384d0aec0c1e
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-07-17 15:26:50 +0700

    First Commit

--- app/src/main/java/com/example/mart/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/mart/msgshareapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mart.msgshareapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,6 +19,15 @@ class MainActivity : AppCompatActivity() {
             //Code
             Log.i("MainActivity","Button was clicked!")
             Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        sendIntent.setOnClickListener {
+            var message: String = editText.text.toString()
+//            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
